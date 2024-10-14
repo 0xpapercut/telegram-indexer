40fb645bba158bd870b9eb38ebd3cbece07962c6
0xpapercut <0xpapercut@gmail.com>
2024-10-14 13:24:20 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,27 +1,5 @@
-CREATE TABLE messages (
-    message_id INTEGER,
-    user_id INTEGER NULL,
-    chat_id INTEGER,
-    text TEXT,
-    date TIMESTAMP,
-    insertion_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (chat_id, message_id),
-    FOREIGN KEY (chat_id) REFERENCES chats(chat_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
-
-CREATE TABLE chats (
-    chat_id INTEGER PRIMARY KEY,
-    title TEXT,
-    is_group BOOLEAN,
-    is_channel BOOLEAN,
-    is_user BOOLEAN,
-    message_id_cursor INTEGER DEFAULT NULL,
-    FOREIGN KEY (message_id_cursor) REFERENCES messages(message_id)
-);
-
 CREATE TABLE users (
-    user_id INTEGER PRIMARY KEY,
+    user_id BIGINT PRIMARY KEY,
     username TEXT,
     first_name TEXT,
     last_name TEXT,
@@ -32,35 +10,33 @@ CREATE TABLE users (
     is_verified BOOLEAN
 );
 
+CREATE TABLE chats (
+    chat_id BIGINT PRIMARY KEY,
+    title TEXT,
+    is_group BOOLEAN,
+    is_channel BOOLEAN,
+    is_user BOOLEAN,
+    latest_historical_message BIGINT DEFAULT NULL
+    -- FOREIGN KEY (latest_historical_message) REFERENCES messages(message_id)
+);
+
+CREATE TABLE messages (
+    message_id BIGINT,
+    user_id BIGINT NULL,
+    chat_id BIGINT,
+    text TEXT,
+    date TIMESTAMP,
+    is_historical BOOLEAN,
+    insertion_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (chat_id, message_id),
+    FOREIGN KEY (chat_id) REFERENCES chats(chat_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
 CREATE TABLE chats_participants_count(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    chat_id INTEGER,
-    participants_count INTEGER,
-    api_time TIMESTAMP,
+    id BIGSERIAL PRIMARY KEY,
+    chat_id BIGINT,
+    participants_count BIGINT,
+    date TIMESTAMP,
     FOREIGN KEY (chat_id) REFERENCES chats(chat_id)
 )
-
--- CREATE TABLE chats_users(
---     chat_id INTEGER,
---     user_id INTEGER,
---     FOREIGN KEY (chat_id) REFERENCES chats(chat_id),
---     FOREIGN KEY (user_id) REFERENCES users(user_id),
---     PRIMARY KEY (chat_id, user_id)
--- );
-
--- CREATE TABLE chats_user_count (
---     chat_id INTEGER PRIMARY KEY,
---     user_count INTEGER,
---     api_time TIMESTAMP,
---     insertion_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---     FOREIGN KEY (chat_id) REFERENCES chats(chat_id)
--- )
-
--- CREATE TABLE chat_actions (
---     id INTEGER PRIMARY KEY AUTOINCREMENT,
---     chat_id INTEGER,
---     user_id INTEGER,
---     api_time TIMESTAMP,
---     event TEXT,
---     insertion_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
