81e7cdffc9fbc4b06cf7f035db92e228923a6ca5
0xpapercut <0xpapercut@gmail.com>
2024-10-15 01:02:02 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -16,27 +16,25 @@ CREATE TABLE chats (
     is_group BOOLEAN,
     is_channel BOOLEAN,
     is_user BOOLEAN,
-    latest_historical_message BIGINT DEFAULT NULL
-    -- FOREIGN KEY (latest_historical_message) REFERENCES messages(message_id)
+    latest_historical_message_id message_id BIGINT NULL,
 );
 
 CREATE TABLE messages (
     message_id BIGINT,
-    user_id BIGINT NULL,
+    sender_id BIGINT,
     chat_id BIGINT,
     text TEXT,
     date TIMESTAMP,
     is_historical BOOLEAN,
     insertion_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (chat_id, message_id),
-    FOREIGN KEY (chat_id) REFERENCES chats(chat_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    FOREIGN KEY (chat_id) REFERENCES chats(chat_id)
 );
 
 CREATE TABLE chats_participants_count(
     id BIGSERIAL PRIMARY KEY,
     chat_id BIGINT,
     participants_count BIGINT,
-    date TIMESTAMP,
+    insertion_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (chat_id) REFERENCES chats(chat_id)
-)
+);
